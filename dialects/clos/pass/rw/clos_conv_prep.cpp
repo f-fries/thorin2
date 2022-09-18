86ba27a5c79f6754119afcee1caf93897fdc50a1
Fabian Fries <s8fafrie@stud.uni-saarland.de>
2022-09-18 19:13:04 +0200

    - Remove EtaExp - refactor get_ifs()/set_ifs(), - distinguish eta-wrapper \w different tags - eta-expand branches - cleanup

--- dialects/clos/pass/rw/clos_conv_prep.cpp
+++ dialects/clos/pass/rw/clos_conv_prep.cpp
@@ -1,102 +1,8 @@
 #include "dialects/clos/pass/rw/clos_conv_prep.h"
 
-#include "thorin/pass/fp/eta_exp.h"
-
 #include "dialects/clos/clos.h"
 
 namespace thorin::clos {
-
-// FIXME: these guys do not work if another pass rewrites curr_nom()'s body
-/* static bool isa_cont(const App* body, const Def* def, size_t i) { *//*{{{*/
-/*     return body->callee_type()->is_returning() && body->arg() == def && i == def->num_ops() - 1; */
-/* } */
-
-/* static const Def* isa_br(const App* body, const Def* def) { */
-/*     if (!body->callee_type()->is_cn()) return nullptr; */
-/*     auto proj = body->callee()->isa<Extract>(); */
-/*     return (proj && proj->tuple() == def && proj->tuple()->isa<Tuple>()) ? proj->tuple() : nullptr; */
-/* } */
-
-/* static bool isa_callee_br(const App* body, const Def* def, size_t i) { */
-/*     if (!body->callee_type()->is_cn()) return false; */
-/*     return isa_callee(def, i) || isa_br(body, def); */
-/* } */
-
-/* static Lam* isa_retvar(const Def* def) { */
-/*     if (auto [var, lam] = ca_isa_var<Lam>(def); var && lam && var == lam->ret_var()) return lam; */
-/*     return nullptr; */
-/* } */
-
-/* Lam* ClosConvPrep::scope(Lam* lam) { */
-/*     if (eta_exp_) lam = eta_exp_->new2old(lam); */
-/*     return lam2fscope_[lam]; */
-/* } */
-
-
-/* const Def* ClosConvPrep::rewrite(const Def* def) { */
-/*     auto& w = world(); */
-/*     if (!cur_body_ || match<clos>(def) || def->isa<Var>()) return def; */
-/*     for (auto i = 0u; i < def->num_ops(); i++) { */
-/*         auto op     = def->op(i); */
-/*         auto refine = [&](const Def* new_op) { */
-/*             auto new_def = def->refine(i, new_op); */
-/*             if (def == cur_body_->callee()) */
-/*                 cur_body_ = cur_body_->refine(0, new_def)->as<App>(); */
-/*             else if (def == cur_body_->arg()) */
-/*                 cur_body_ = cur_body_->refine(1, new_def)->as<App>(); */
-/*             else if (isa_br(cur_body_, def)) */
-/*                 cur_body_ = cur_body_->refine(0, cur_body_->callee()->refine(0, new_def))->as<App>(); */
-/*             return new_def; */
-/*         }; */
-/*         if (auto lam = isa_retvar(op); lam && from_outer_scope(lam)) { */
-/*             w.DLOG("found return var from enclosing scope: {}", op); */
-/*             return refine(eta_wrap(op, clos::freeBB, "free_ret")); */
-/*         } */
-/*         if (auto bb_lam = op->isa_nom<Lam>(); bb_lam && bb_lam->is_basicblock() && from_outer_scope(bb_lam)) { */
-/*             w.DLOG("found BB from enclosing scope {}", op); */
-/*             return refine(thorin::clos::op(clos::freeBB, op)); */
-/*         } */
-/*         if (isa_cont(cur_body_, def, i)) { */
-/*             if (match<clos>(clos::ret, op) || isa_retvar(op)) { */
-/*                 return def; */
-/*             } else if (auto contlam = op->isa_nom<Lam>()) { */
-/*                 return refine(thorin::clos::op(clos::ret, contlam)); */
-/*             } else { */
-/*                 auto wrapper = eta_wrap(op, clos::ret, "eta_cont"); */
-/*                 w.DLOG("eta expanded return cont: {} -> {}", op, wrapper); */
-/*                 return refine(wrapper); */
-/*             } */
-/*         } */
-/*         if (auto bb_lam = op->isa_nom<Lam>(); bb_lam && bb_lam->is_basicblock() && !isa_callee_br(cur_body_, def, i)) { */
-/*             w.DLOG("found firstclass use of BB: {}", bb_lam); */
-/*             return refine(thorin::clos::op(clos::fstclassBB, bb_lam)); */
-/*         } */
-/*         // TODO: If EtaRed eta-reduces branches, we have to wrap them again! */
-/*         if (isa_retvar(op) && !isa_callee_br(cur_body_, def, i)) { */
-/*             w.DLOG("found firstclass use of return var: {}", op); */
-/*             return refine(eta_wrap(op, clos::fstclassBB, "fstclass_ret")); */
-/*         } */
-/*     } */
-
-/*     // Eta-Expand branches */
-/*     if (auto app = def->isa<App>(); app && app->callee_type()->is_cn()) { */
-/*         auto br = app->callee()->isa<Extract>(); */
-/*         if (!br) return def; */
-/*         auto branches = br->tuple(); */
-/*         if (!branches->isa<Tuple>() || !branches->type()->isa<Sigma>()) return def; */
-/*         for (auto i = 0u; i < branches->num_ops(); i++) { */
-/*             if (!branches->op(i)->isa_nom<Lam>()) { */
-/*                 auto wrapper = eta_wrap(branches->op(i), clos::bot, "eta_br"); */
-/*                 w.DLOG("eta wrap branch: {} -> {}", branches->op(i), wrapper); */
-/*                 branches = branches->refine(i, wrapper); */
-/*             } */
-/*         } */
-/*         cur_body_ = app->refine(0, app->callee()->refine(0, branches))->as<App>(); */
-/*         return cur_body_; */
-/*     } */
-
-/*     return def; */
-/* } *//*}}}*/
 
 static Lam* is_bb(const Def* def) {
     auto bb = def->isa_nom<Lam>();
@@ -124,6 +30,14 @@ void ClosConvPrep::AnnotBr::enter() {
     }
 }
 
+const Def* ClosConvPrep::AnnotBr::rewrite(const Def* old_def) {
+    // We really only need to expand retvars in branches but this simpliefies the other rules.
+    if (auto br = match(clos::br, old_def); br && !br->arg()->isa_nom<Lam>()) {
+        return annot_ext_->eta_wrap(clos::br, br->arg(), "eta_br");
+    }
+    return old_def;
+}
+
 void ClosConvPrep::AnnotRet::enter() {
     if (auto app = curr_nom()->body()->isa<App>(); app && app->callee_type()->is_returning()) {
         auto n        = app->num_args() - 1;
@@ -135,45 +49,33 @@ void ClosConvPrep::AnnotRet::enter() {
     }
 }
 
-const Def* ClosConvPrep::AnnotRet::rewrite(const Def* def) {
-    if (auto retcn = match(clos::ret, def)) {
-        if (retcn->arg()->isa_nom<Lam>() || is_retvar_of(retcn->arg())) return def;
+const Def* ClosConvPrep::AnnotRet::rewrite(const Def* old_def) {
+    if (auto retcn = match(clos::ret, old_def)) {
+        if (retcn->arg()->isa_nom<Lam>() || is_retvar_of(retcn->arg())) return old_def;
         return annot_ext_->eta_wrap(clos::ret, retcn->arg(), "eta_ret");
     }
-    return def;
+    return old_def;
 }
 
 bool ClosConvPrep::AnnotExt::is_free_bb(const Def* def) { 
     Lam* ifs = is_retvar_of(def);
     if (!ifs) {
-        if (auto bb = is_bb(def)) ifs = bb2ifs(bb);
+        if (auto bb = is_bb(def)) ifs = get_ifs(bb);
     }
 
-    // There are 1 possible cases if !ifs:
+    // There are two possible cases if !ifs:
     // (1) def is neither a BB-lam or retvar; 
-    // (b) def's ifs has not been visited => def is a toplevel (free & closed) BB-like lam;
-    return ifs && !ext_wrapper_.contains(curr_nom()) && ifs != bb2ifs(curr_nom());
+    // (b) def's ifs has not been visited => def is a toplevel (free & closed) BB-like lam.
+    return ifs && !ext_wrapper_.contains(curr_nom()) && ifs != get_ifs(curr_nom());
 }
 
 void ClosConvPrep::AnnotExt::enter() {
     if (curr_nom()->type()->is_returning() && !visited_fncs_.contains(curr_nom())) {
-        visited_fncs_.emplace(curr_nom());
-        bb2ifs_[curr_nom()] = curr_nom();
-        world().DLOG("scope {} -> {}", curr_nom(), curr_nom());
+        visited_fncs_.emplace(curr_nom()); // We only do this once since we track all new lam's via set_ifs(). 
         auto scope = Scope(curr_nom());
-        for (auto def : scope.bound()) {
-            assert(def);
-            if (auto bb_lam = def->isa_nom<Lam>(); bb_lam && bb_lam->is_basicblock()) {
-                world().DLOG("scope {} -> {}", bb_lam, curr_nom());
-                bb2ifs_[bb_lam] = curr_nom();
-            }
-        }
+        for (auto def : scope.bound())
+            if (auto bb_lam = def->isa_nom<Lam>(); bb_lam && bb_lam->is_basicblock()) set_ifs(bb_lam);
     }
-    // if (auto body = curr_nom()->body()->isa<App>();
-    //     !wrapper_.contains(curr_nom()) && body && body->callee_type()->is_cn())
-    //     cur_body_ = body;
-    // else
-    //     cur_body_ = nullptr;
 }
 
 const Def* ClosConvPrep::AnnotExt::rewrite(const Def* old_def) {
@@ -189,8 +91,8 @@ const Def* ClosConvPrep::AnnotExt::rewrite(const Def* old_def) {
             w.DLOG("found firstclass bb: {}", op);
             return old_def->refine(i, eta_wrap(clos::ext, op, "eta_ext"));
         }
-        if (auto _ = is_retvar_of(op)) {
-            w.DLOG("found firstclass bb: {}", op);
+        if (is_retvar_of(op)) {
+            w.DLOG("found firstclass retvar: {}", op);
             return old_def->refine(i, eta_wrap(clos::ext, op, "eta_ext"));
         }
     }
--- dialects/clos/pass/rw/clos_conv_prep.h
+++ dialects/clos/pass/rw/clos_conv_prep.h
@@ -3,117 +3,96 @@
 #include "thorin/pass/pass.h"
 #include "thorin/phase/phase.h"
 
-#include "thorin/pass/fp/eta_exp.h"
-
 #include "dialects/clos/clos.h"
 
 namespace thorin {
 
 namespace clos {
 
-// class ClosConvPrep : public RWPass<ClosConvPrep, Lam> {{{{
-// public:
-//     ClosConvPrep(PassMan& man, EtaExp* eta_exp)
-//         : RWPass(man, "eta_cont")
-//         , eta_exp_(eta_exp)
-//         , old2wrapper_()
-//         , lam2fscope_()
-//         , cur_body_(nullptr) {}
-
-//     void enter() override;
-//     const Def* rewrite(const Def*) override;
-
-//     Lam* scope(Lam* lam);
-
-//     bool from_outer_scope(Lam* lam) { return scope(lam) && scope(lam) != scope(curr_nom()); }
-
-//     const Def* eta_wrap(const Def* def, clos c, const std::string& dbg) {
-//         auto& w                = world();
-//         auto [entry, inserted] = old2wrapper_.emplace(def, nullptr);
-//         auto& wrapper          = entry->second;
-//         if (inserted) {
-//             wrapper = w.nom_lam(def->type()->as<Pi>(), w.dbg(dbg));
-//             wrapper->app(false, def, wrapper->var());
-//             lam2fscope_[wrapper] = scope(curr_nom());
-//             wrapper_.emplace(wrapper);
-//         }
-//         return op(c, wrapper);
-//     }
-
-// private:
-//     EtaExp* eta_exp_;
-//     DefMap<Lam*> old2wrapper_;
-//     DefSet wrapper_;
-//     Lam2Lam lam2fscope_;
-//     const App* cur_body_;
-// };}}}
-
 class ClosConvPrep : public Pipeline {
 public:
 
+    /// Adds the passes to a PassMan.
+    /// IMPORTANT: DO NOT COMBINE THIS WITH OTHER PASSES!
     static void addPasses(PassMan& man) {
-        man.add<AnnotBr>();
-        auto eta_exp = man.add<EtaExp>(nullptr);
-        auto annot_ext = man.add<AnnotExt>(eta_exp);
+        auto annot_ext = man.add<AnnotExt>();
+        man.add<AnnotBr>(annot_ext);
         man.add<AnnotRet>(annot_ext);
     }
 
     ClosConvPrep(World& w)
         : Pipeline(w) {
         auto man = std::make_unique<PassMan>(w);
-        // addPasses(*man); 
+        addPasses(*man); 
         add<PassManPhase>(std::move(man));
     }
 
 protected:
-
-    class AnnotBr : public RWPass<AnnotBr, Lam> {
-    public:
-        AnnotBr(PassMan& man)
-            : RWPass(man, "annot_br") {}
-
-        void enter() override;
-    };
-
     class AnnotExt : public RWPass<AnnotExt, Lam> {
     public:
-        AnnotExt(PassMan& man, EtaExp* eta_exp)
+        AnnotExt(PassMan& man)
             : RWPass(man, "annot_ext") 
-            , eta_exp_(eta_exp) 
             , old2wrapper_()
             , visited_fncs_()
             , ext_wrapper_() {};
 
-        void enter() override;
+    public:
+        /// Get the innermost enclosing function scope (IFS) for lam.
+        /// Note: This may be `nullptr` if @p lam is not contained in any function scope, i.e. lam is as BB and closed.
+        Lam* get_ifs(Lam* lam) {
+            if (lam->is_returning()) return lam;
+            return bb2ifs_[lam];
+        }
 
-        const Def* rewrite(const Def* old_def) override;
-        
+        /// Set bblam%s IFS to the IFS of `curr_nom()`.
+        /// We need to track the IFS of all continuations that are created. 
+        void set_ifs(Lam* bblam) {
+            auto ifs = get_ifs(curr_nom());
+            bb2ifs_[bblam] = ifs;
+            world().DLOG("scope {} -> {}", bblam, ifs);
+        }
+
+        /// η-expand def and tag the wrapper with c. 
         auto eta_wrap(clos c, const Def* def, const std::string& dbg = {}) {
             auto& w = def->world();
-            auto [entry, inserted] = old2wrapper_.emplace(def, nullptr);
+            auto [entry, inserted] = old2wrapper_.emplace(op(c, def), nullptr);
             auto& wrapper = entry->second;
             if (inserted) {
                 wrapper = w.nom_lam(def->type()->as<Pi>(), w.dbg(dbg));
                 wrapper->as_nom<Lam>()->app(false, def, wrapper->as_nom<Lam>()->var());
-                bb2ifs_[wrapper->as_nom<Lam>()] = bb2ifs(curr_nom());
+                w.DLOG("η-expand: {} -> {}", def, op(c, wrapper));
+                set_ifs(wrapper->as_nom<Lam>());
                 if (c == clos::ext) ext_wrapper_.emplace(wrapper->as_nom<Lam>());
             }
             return op(c, wrapper);
         }
 
-        Lam* bb2ifs(Lam* bb) {
-            bb = eta_exp_->new2old(bb);
-            return bb2ifs_[bb];
-        }
-
         bool is_free_bb(const Def* def);
 
+        /// @name PassMan hooks 
+        /// @{
+        void enter() override;
+        const Def* rewrite(const Def* old_def) override;
+        /// @}
+
     private:
-        EtaExp* eta_exp_;
-        Def2Def old2wrapper_;
-        Lam2Lam bb2ifs_;
+        Def2Def old2wrapper_; 
+        Lam2Lam bb2ifs_;      
         LamSet visited_fncs_;
-        LamSet ext_wrapper_;
+        LamSet ext_wrapper_;  ///< η-wrapper for nonlocal BB. These are ignored when computing free BBs.
+    };
+
+    class AnnotBr : public RWPass<AnnotBr, Lam> {
+    public:
+        AnnotBr(PassMan& man, AnnotExt* annot_ext)
+            : RWPass(man, "annot_br") 
+            , annot_ext_(annot_ext) {}
+
+        void enter() override;
+        const Def* rewrite(const Def* old_def) override;
+
+    private:
+        AnnotExt* annot_ext_;
     };
 
     class AnnotRet : public RWPass<AnnotRet, Lam> {
@@ -123,11 +102,9 @@ protected:
             , annot_ext_(annot_ext) {};
 
         void enter() override;
-
         const Def* rewrite(const Def* old) override;
 
     private:
-        Def2Def old2wrapper_;
         AnnotExt* annot_ext_;
     };
 };
